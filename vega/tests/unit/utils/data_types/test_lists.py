d44698065ada069943568d55743561edaa2be55a
Philipp Wiesner <philipwiesner@gmail.com>
2020-04-28 22:55:21 +0200

    start using pytest for testing framework

--- vega/tests/unit/utils/data_types/test_lists.py
+++ vega/tests/unit/utils/data_types/test_lists.py
@@ -1,125 +1,146 @@
-import unittest
-from vega.utils.data_types.lists import MetaList, Stack, Queue
+import pytest
+from utils.data_types.lists import MetaList, Stack, Queue
 
 
-class TestMetaList(unittest.TestCase):
+@pytest.fixture
+def meta_list():
+    return MetaList()
 
-    def setUp(self):
-        self.meta_list: MetaList = MetaList()
 
-    def test_initialization(self):
-        self.assertEqual(self.meta_list.tail, None)
-        self.assertEqual(self.meta_list.head, None)
-        self.assertEqual(len(self.meta_list), 0)
+@pytest.fixture()
+def stack():
+    return Stack()
 
-    def test_empty_list(self):
-        self.assertEqual(self.meta_list.is_empty(), True)
 
-    def test_private_methods(self):
-        with self.assertRaises(AttributeError):
-            count: int = self.meta_list.__count
-            head: object = self.meta_list.__head
-            tail: object = self.meta_list.__tail
+@pytest.fixture()
+def queue():
+    return Queue()
 
-    def test_setter_methods(self):
+
+class TestMetaList:
+
+    def test_initialization(self, meta_list):
+        assert meta_list.tail is None
+        assert meta_list.head is None
+        assert len(meta_list) == 0
+
+    def test_empty_list(self, meta_list):
+        assert meta_list.is_empty() is True
+
+    def test_private_methods(self, meta_list):
+        with pytest.raises(AttributeError):
+            count: int = meta_list.__count
+            head: object = meta_list.__head
+            tail: object = meta_list.__tail
+
+    def test_setter_methods(self, meta_list):
         node = 5
-        self.meta_list.head = node
-        self.meta_list.tail = node
-        self.assertEqual(self.meta_list.head, node)
-        self.assertEqual(self.meta_list.tail, node)
 
-    def test_object_representation(self):
-        self.assertEqual(self.meta_list.__repr__(), 'MetaList(0)')
+        meta_list.head = node
+        meta_list.tail = node
+        assert meta_list.head == node
+        assert meta_list.tail == node
 
-    def test_string_representation(self):
-        self.assertEqual(self.meta_list.__str__(), '0')
+    def test_object_representation(self, meta_list):
+        assert meta_list.__repr__() == 'MetaList(0)'
 
-
-class TestStack(unittest.TestCase):
-
-    def setUp(self) -> None:
-        self.stack = Stack()
-
-    def test_initialization(self):
-        self.assertEqual(len(self.stack), 0)
-
-    def test_add_one_element(self):
-        self.stack.push(1)
-
-        self.assertEqual(len(self.stack), 1)
-        self.assertEqual(self.stack.tail.data, 1)
-        self.assertEqual(self.stack.head.data, 1)
-
-    def test_add_two_elements(self):
-        self.stack.push(1)
-        self.stack.push(5)
-
-        self.assertEqual(len(self.stack), 2)
-        self.assertEqual(self.stack.head.data, 5)
-        self.assertEqual(self.stack.tail.data, 1)
-        self.assertEqual(self.stack.head.prev.data, 1)
-        self.assertEqual(self.stack.tail.next.data, 5)
-        self.assertEqual(self.stack.tail.prev, None)
-        self.assertEqual(self.stack.head.next, None)
-
-    def test_remove_element_on_empty(self):
-        with self.assertRaises(IndexError):
-            self.stack.pop()
-
-    def test_remove_element(self):
-        self.stack.push(1)
-        self.stack.push(5)
-        self.stack.push(7)
-
-        element: int = self.stack.pop()
-        self.assertEqual(element, 7)
-        self.assertEqual(len(self.stack), 2)
-        self.assertEqual(self.stack.head.data, 5)
-        self.assertEqual(self.stack.head.next, None)
+    def test_string_representation(self, meta_list):
+        assert meta_list.__str__() == '0'
 
 
-class TestQueue(unittest.TestCase):
+class TestStack:
 
-    def setUp(self) -> None:
-        self.queue = Queue()
+    def test_initialization(self, stack):
+        assert len(stack) == 0
 
-    def test_initialization(self):
-        self.assertEqual(len(self.queue), 0)
+    def test_add_one_element(self, stack):
+        stack.push(1)
 
-    def test_add_one_element(self):
-        self.queue.add(1)
+        assert len(stack) == 1
+        assert stack.tail.data == 1
+        assert stack.head.data == 1
 
-        self.assertEqual(len(self.queue), 1)
-        self.assertEqual(self.queue.tail.data, 1)
-        self.assertEqual(self.queue.head.data, 1)
+    @pytest.mark.parametrize("add_one, add_two, add_three, count", [
+        (1, 5, 4, 3),
+        (9, 5, 0, 3)
+    ])
+    def test_add_multiple_elements(self, stack, add_one, add_two, add_three, count):
+        stack.push(add_one)
+        stack.push(add_two)
+        stack.push(add_three)
 
-    def test_add_two_elements(self):
-        self.queue.add(1)
-        self.queue.add(5)
+        assert len(stack) == count
+        assert stack.head.data == add_three
+        assert stack.tail.data == add_one
+        assert stack.head.prev.data == add_two
+        assert stack.tail.next.data == add_two
+        assert stack.head.next is None
+        assert stack.tail.prev is None
 
-        self.assertEqual(len(self.queue), 2)
-        self.assertEqual(self.queue.tail.data, 5)
-        self.assertEqual(self.queue.head.data, 1)
-        self.assertEqual(self.queue.tail.next.data, 1)
-        self.assertEqual(self.queue.head.prev.data, 5)
-        self.assertEqual(self.queue.tail.prev, None)
-        self.assertEqual(self.queue.head.next, None)
+    def test_remove_element_on_empty(self, stack):
+        with pytest.raises(IndexError):
+            stack.pop()
 
-    def test_remove_element_on_empty(self):
-        with self.assertRaises(IndexError):
-            self.queue.remove()
+    @pytest.mark.parametrize("add_one, add_two, add_three, count", [
+        (5, 78, 8, 2),
+        (1, 7, 56, 2)
+    ])
+    def test_remove_element(self, stack, add_one, add_two, add_three, count):
+        stack.push(add_one)
+        stack.push(add_two)
+        stack.push(add_three)
 
-    def test_remove_element(self):
-        self.queue.add(1)
-        self.queue.add(5)
-        self.queue.add(7)
-
-        element: int = self.queue.remove()
-        self.assertEqual(element, 1)
-        self.assertEqual(len(self.queue), 2)
-        self.assertEqual(self.queue.head.data, 5)
-        self.assertEqual(self.queue.head.next, None)
+        element: int = stack.pop()
+        assert element == add_three
+        assert len(stack) == 2
+        assert stack.head.data == add_two
+        assert stack.head.next is None
 
 
-if __name__ == '__main__':
-    unittest.main()
+class TestQueue:
+
+    def test_initialization(self, queue):
+        assert len(queue) == 0
+
+    def test_add_one_element(self, queue):
+        queue.add(1)
+
+        assert len(queue) == 1
+        assert queue.tail.data == 1
+        assert queue.head.data == 1
+
+    @pytest.mark.parametrize("add_one, add_two, add_three, count", [
+        (1, 5, 4, 3),
+        (9, 5, 0, 3)
+    ])
+    def test_add_multiple_elements(self, queue, add_one, add_two, add_three, count):
+        queue.add(add_one)
+        queue.add(add_two)
+        queue.add(add_three)
+
+        assert len(queue) == count
+        assert queue.tail.data == add_three
+        assert queue.head.data == add_one
+        assert queue.tail.next.data == add_two
+        assert queue.head.prev.data == add_two
+        assert queue.tail.prev is None
+        assert queue.head.next is None
+
+    def test_remove_element_on_empty(self, queue):
+        with pytest.raises(IndexError):
+            queue.remove()
+
+    @pytest.mark.parametrize("add_one, add_two, add_three, count", [
+        (5, 78, 8, 2),
+        (1, 7, 56, 2)
+    ])
+    def test_remove_element(self, queue, add_one, add_two, add_three, count):
+        queue.add(add_one)
+        queue.add(add_two)
+        queue.add(add_three)
+
+        element: int = queue.remove()
+        assert element == add_one
+        assert len(queue) == count
+        assert queue.head.data == add_two
+        assert queue.head.next is None
