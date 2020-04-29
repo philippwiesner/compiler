c6a93559765e737bdb9322b896a13f45a4298d49
Philipp Wiesner <philipwiesner@gmail.com>
2020-04-29 21:56:06 +0200

    use pytest for hashtable tests as well

--- tests/unit/data_types/test_hashTable.py
+++ tests/unit/data_types/test_hashTable.py
@@ -1,94 +1,102 @@
-import unittest
-from unittest.mock import MagicMock
+import pytest
 from vega.utils.data_types.hashTable import Bucket, HashTable
 
 
-class TestBucket(unittest.TestCase):
+@pytest.fixture
+def bucket():
+    return Bucket('foo', 5)
 
-    def setUp(self):
-        self.bucket = Bucket(key='foo', data=5)
 
-    def test_iterator(self):
+def mock_gen_hash(*args, **kwargs):
+    return 10
+
+
+@pytest.fixture(autouse=True)
+def hash_table(monkeypatch):
+    monkeypatch.setattr(HashTable, "_HashTable__gen_hash", mock_gen_hash)
+    h = HashTable()
+    return h
+
+
+class TestBucket:
+
+    def test_iterator(self, bucket):
         b = Bucket('bar', 3)
-        self.bucket.next = b
+        bucket.next = b
 
-        elements = [i.data for i in self.bucket]
+        elements = [i.data for i in bucket]
 
-        self.assertEqual(elements, [5, 3])
+        assert elements == [5, 3]
 
-    def test_chaining(self):
+    def test_chaining(self, bucket):
         b = Bucket('bar', 3)
-        c = self.bucket + b
+        c = bucket + b
 
-        self.assertEqual(c.next, b)
+        assert c.next == b
 
-    def test_overwriting_existing_element(self):
-        a = Bucket('blubb', 12)
-        b = Bucket('bar', 6)
+    @pytest.mark.parametrize("first, second, overwrite", [
+        (12, 6, "hello world"),
+        (9, 23, "foobar")
+    ])
+    def test_overwriting_existing_element(self, bucket, first, second, overwrite):
+        a = Bucket('blubb', first)
+        b = Bucket('bar', second)
 
-        c = self.bucket + a + b
+        c = bucket + a + b
 
-        self.assertEqual(c.next.data, 12)
-        self.assertEqual(c.next.next.data, 6)
+        assert c.next.data == first
+        assert c.next.next.data == second
 
-        x = Bucket('blubb', 'hello world')
-        c = self.bucket + x
+        x = Bucket('blubb', overwrite)
+        c = bucket + x
 
-        self.assertEqual(c.next.data, 'hello world')
-        self.assertEqual(c.next.next.data, 6)
-
-
-class TestHashTable(unittest.TestCase):
-
-    class TestData:
-        def __init__(self, name: str, data: str):
-            self.name = name
-            self.data = data
-
-    def setUp(self) -> None:
-        self.hashTable = HashTable()
-        self.testData1 = TestHashTable.TestData('foo', 'Hello World')
-        self.testData2 = TestHashTable.TestData('bar', 'Hello Jupiter')
-
-    def test_store_data(self):
-        self.hashTable._HashTable__gen_hash = MagicMock(return_value=10)
-        self.hashTable.put(self.testData1.name, self.testData1.data)
-
-        self.assertEqual(self.hashTable._HashTable__data[10].data, 'Hello World')
-        self.assertEqual(len(self.hashTable), 1)
-
-    def test_collision(self):
-        self.hashTable._HashTable__gen_hash = MagicMock(return_value=10)
-        self.hashTable.put(self.testData1.name, self.testData1.data)
-        self.hashTable.put(self.testData2.name, self.testData2.data)
-
-        self.assertEqual(self.hashTable._HashTable__data[10].data, 'Hello World')
-        self.assertEqual(self.hashTable._HashTable__data[10].next.data, 'Hello Jupiter')
-        self.assertEqual(len(self.hashTable), 2)
-
-    def test_element_overwrite(self):
-        self.hashTable._HashTable__gen_hash = MagicMock(return_value=10)
-        self.hashTable.put(self.testData1.name, self.testData1.data)
-
-        self.assertEqual(self.hashTable._HashTable__data[10].data, 'Hello World')
-
-        self.hashTable.put(self.testData1.name, 'Hello Mars')
-
-        self.assertEqual(self.hashTable._HashTable__data[10].data, 'Hello Mars')
-
-    def test_element_retrieval(self):
-        self.hashTable.put(self.testData1.name, self.testData1.data)
-
-        self.assertEqual(self.hashTable.get('foo'), 'Hello World')
-
-    def test_element_retrieval_after_collision(self):
-        self.hashTable._HashTable__gen_hash = MagicMock(return_value=10)
-        self.hashTable.put(self.testData1.name, self.testData1.data)
-        self.hashTable.put(self.testData2.name, self.testData2.data)
-
-        self.assertEqual(self.hashTable.get('foo'), 'Hello World')
-        self.assertEqual(self.hashTable.get('bar'), 'Hello Jupiter')
+        assert c.next.data == overwrite
+        assert c.next.next.data == second
 
 
-if __name__ == '__main__':
-    unittest.main()
+@pytest.mark.parametrize("first, second", [
+    ("while", "if"),
+    ("variable1", "variable2")
+])
+class TestHashTable:
+
+    def test_store_data(self, hash_table, first, second):
+
+        hash_table.put(first, first)
+
+        assert hash_table._HashTable__data[10].data == first
+        assert len(hash_table) == 1
+
+    def test_collision(self, hash_table, first, second):
+        hash_table.put(first, first)
+        hash_table.put(second, second)
+
+        assert hash_table._HashTable__data[10].data == first
+        assert hash_table._HashTable__data[10].next.data == second
+        assert len(hash_table) == 2
+
+    def test_element_overwrite(self, hash_table, first, second):
+
+        hash_table.put(first, first)
+
+        assert hash_table._HashTable__data[10].data == first
+
+        hash_table.put(first, second)
+
+        assert hash_table._HashTable__data[10].data == second
+
+    def test_element_retrieval(self, first, second):
+        hash_table = HashTable()
+        hash_table.put(first, first)
+        hash_table.put(second, second)
+
+        assert hash_table.get(first) == first
+        assert hash_table.get(second) == second
+
+    def test_element_retrieval_after_collision(self, hash_table, first, second):
+
+        hash_table.put(first, first)
+        hash_table.put(second, second)
+
+        assert hash_table.get(first) == first
+        assert hash_table.get(second) == second
