7370ec6ec0fb466b054a400586001b98dfd9c940
Philipp Wiesner <philipwiesner@gmail.com>
2020-05-06 00:35:55 +0200

    remove old stuff from main

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
-from front_end.lexer import Lexer
+from vega.front_end.lexer import Lexer
 from argparse import ArgumentParser, FileType
 
-parser = ArgumentParser(description="Compile")
-parser.add_argument('code', type=FileType('r'))
-args = parser.parse_args()
+if __name__ == 'main':
+    parser = ArgumentParser(description="Compile")
+    parser.add_argument('code', type=FileType('r'))
+    args = parser.parse_args()
 
-code = args.code
+    code = args.code
 
-l = Lexer(code)
-
-for i in range(200):
-    print(repr(l.scan()))
+    lexer = Lexer(code)
+    token_stream = lexer.scan()
