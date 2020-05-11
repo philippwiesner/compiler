4d14f0561dc29100b20f6adfeb83f91d3612f6ed
Philipp Wiesner <philipwiesner@gmail.com>
2020-05-11 20:53:44 +0200

    fully test symbol table

--- tests/unit/vega/symbol/test_symbol_table.py
+++ tests/unit/vega/symbol/test_symbol_table.py
@@ -1,3 +1,4 @@
+# pylint: skip-file
 import pytest
 
 from vega.symbol.symbol_table import SymbolTable
@@ -7,19 +8,25 @@ from vega.language.vocabulary import Tag
 
 def describe_symbol_table():
     @pytest.fixture
-    def symbol_table() -> SymbolTable:
-        table = SymbolTable()
-        table.store("A", True, INT, Tag.BASIC)
-        table.store("text", False, CHAR, Tag.INDEX)
-        return table
+    def symbol_table():
+        symbol_table = SymbolTable()
+        symbol_table.store("A", True, INT, Tag.BASIC)
+        symbol_table.store("text", False, CHAR, Tag.INDEX)
+        symbol_table.enter_scope()
+        symbol_table.store("do_something", False, INT, Tag.FUNCTION)
+        return symbol_table
 
     def describe_lookup():
-        @pytest.mark.parametrize(
-            "name",
-            [
-                pytest.param("A", id="const_integer"),
-                pytest.param("text", id="string"),
-            ]
-        )
-        def describe_global_scope(symbol_table, name):
-            assert symbol_table.lookup(name) is True
+
+        def global_scope(symbol_table):
+
+            assert symbol_table.lookup("A") is True
+            assert symbol_table.lookup("do_something") is True
+            assert symbol_table.lookup("not_present") is False
+
+        def after_leaving_scope(symbol_table):
+            symbol_table.leave_scope()
+
+            assert symbol_table.lookup("A") is True
+            assert symbol_table.lookup("do_something") is False
+            assert symbol_table.lookup("not_present") is False
