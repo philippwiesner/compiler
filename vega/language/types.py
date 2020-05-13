6e69ea13900469136e13a040e831077204c7b607
Philipp Wiesner <philipwiesner@gmail.com>
2020-05-14 01:36:44 +0200

    refactor array type for better multi dimensional handling

--- vega/language/types.py
+++ vega/language/types.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from vega.language.token import Tag
 from vega.language.token import Word
 
@@ -6,7 +8,7 @@ class Type(Word):
 
     def __init__(self, token_type: str, tag: Tag, width: int) -> None:
         super().__init__(token_type, tag)
-        self.__width = width
+        self.__width: int = width
 
     @property
     def width(self) -> int:
@@ -21,27 +23,33 @@ BOOL = Type("bool", Tag.BASIC, 1)
 
 class Array(Type):
 
-    def __init__(self, size: int, token_type: Type) -> None:
-        super().__init__(f'{token_type}[]', Tag.INDEX, size * token_type.width)
-        self.__size = size
-        self.__type = token_type
+    def __init__(self, token_type: Type, **kwargs) -> None:
+        self.__size: int = kwargs.get('size', 0)
+        self.__dimensions: List = [self.__size]
+        self.__type: Type = token_type
+        if isinstance(token_type, Array):
+            self.__dimensions = token_type.dimensions + self.__dimensions
+            self.__type = token_type.type
+        super().__init__('[]',
+                         Tag.INDEX,
+                         self.__size*token_type.width)
 
     @property
-    def size(self) -> int:
-        return self.__size
+    def dimensions(self) -> List:
+        return self.__dimensions
 
     @property
     def type(self) -> Type:
         return self.__type
 
     def __repr__(self) -> str:
-        return f'{self.__class__.__name__}({self.type}[{self.size!r}])'
+        return f'{self.__class__.__name__}({self.type}{self.dimensions!r})'
 
     def __str__(self) -> str:
-        return f'{self.type}[{self.size}]'
+        return f'{self.type}{self.dimensions}'
 
 
 class String(Array):
 
-    def __init__(self, size: int):
-        super().__init__(size, CHAR)
+    def __init__(self, **kwargs):
+        super().__init__(CHAR, **kwargs)
