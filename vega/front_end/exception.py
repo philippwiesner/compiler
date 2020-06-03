e49903f686f5b56a4099f8448bd6ef93b9ab82cb
Philipp Wiesner <philipwiesner@gmail.com>
2020-06-03 16:21:31 +0200

    refactor exceptionss

--- vega/front_end/exception.py
+++ vega/front_end/exception.py
@@ -4,57 +4,56 @@
 class BaseError(Exception):
     """Base class for exceptions in this module"""
 
+    def __init__(self, line, message) -> None:
+        self.line: int = line
+        self.message: str = message
+        super().__init__()
+
 
 class VegaSyntaxError(BaseError):
     """Syntax Error"""
 
     def __init__(self, token, line) -> None:
-        self.token = token
-        self.line = line
-        self.message = f'Invalid Syntax at {self.token} in line {self.line}.'
-        super().__init__()
+        super().__init__(line, message=f'Invalid Syntax at {token} in '
+                                       f'line {line}.')
 
 
 class VegaIdentifierBaseError(BaseError):
     """Variable definiton base error"""
 
-    def __init__(self, identifier, line) -> None:
+    def __init__(self, line, message, identifier) -> None:
         self.identifier = identifier
-        self.line = line
-        super().__init__()
+        super().__init__(line, message)
 
 
 class VegaAlreadyDefinedError(VegaIdentifierBaseError):
     """Variable definition error"""
 
     def __init__(self, identifier, line) -> None:
-        self.message = f'Identifier {identifier} at line {self.line} ' \
-                       f'already defined'
-        super().__init__(identifier, line)
+        message = f'Identifier {identifier} at line {line} already defined'
+        super().__init__(line, message, identifier)
 
 
 class VegaNotYetDefinedError(VegaIdentifierBaseError):
     """Variable definition error"""
 
     def __init__(self, identifier, line) -> None:
-        self.message = f'Identifier {identifier} at line {self.line} ' \
-                       f'not defined'
-        super().__init__(identifier, line)
+        message = f'Identifier {identifier} at line {line} not defined'
+        super().__init__(line, message, identifier)
 
 
 class VegaNoCallableError(VegaIdentifierBaseError):
     """Identifier no callable"""
 
     def __init__(self, identifier, line) -> None:
-        self.message = f'Identifier {identifier} at line {self.line} ' \
-                       f'can not be called'
-        super().__init__(identifier, line)
+        message = f'Identifier {identifier} at line {line} can not be called'
+        super().__init__(line, message, identifier)
 
 
 class VegaNotAssignError(VegaIdentifierBaseError):
     """Identifier cannot be used for assignment"""
 
     def __init__(self, identifier, line) -> None:
-        self.message = f'Identifier {identifier} at line {self.line} ' \
-                       f'cannot used for assignment'
-        super().__init__(identifier, line)
+        message = f'Identifier {identifier} at line {line} cannot used for ' \
+                  f'assignment'
+        super().__init__(line, message, identifier)
